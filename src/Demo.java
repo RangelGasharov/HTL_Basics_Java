1e2f8dc6646bb9d42375edea01cc6332a4f1776b
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-02-25 11:04:47 +0100

    first commit test

---
+++ src/Demo.java
@@ -0,0 +1,22 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+
+        int a = 3;
+        int b = 45;
+        int c = 8;
+        int d = 5;
+
+        if (a==3) {
+            System.out.println("The variable a is 3.");
+        } else if (a>3) {
+            System.out.println("The variable a is bigger than 3");
+        } else {
+            System.out.println("The variable a is smaller than 3");
+        }
+        for (int i = 20; i >= 0;i--){
+            System.out.println("i " + i);
+        }
+    }
+}
