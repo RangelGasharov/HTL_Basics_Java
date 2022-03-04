70af78ae28b64ac9af00fbb343e82b0665b5566c
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-03-04 13:38:16 +0100

    Loop #1

---
+++ src/at/rangel/projects/Loop_1.java
@@ -0,0 +1,13 @@
+package at.rangel.projects;
+
+import java.util.Random;
+
+public class Loop_1 {
+    public static void main(String[] args) {
+
+        for(int i = 10; i >0; i--){
+            System.out.println("Hello " + i);
+        }
+
+    }
+}
