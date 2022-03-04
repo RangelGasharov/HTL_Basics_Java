082e517d4338b6dd560546620d7cf270ec5cf5d7
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-03-04 13:40:25 +0100

    Loop #2

---
+++ src/at/rangel/projects/Loop_2.java
@@ -0,0 +1,13 @@
+package at.rangel.projects;
+
+import java.util.Random;
+
+public class Loop_2 {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+
+    }
+}
