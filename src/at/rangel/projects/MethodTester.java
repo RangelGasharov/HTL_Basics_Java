c35493256cb5ec17d36f5034b5fcc086eee05999
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-03-18 17:33:17 +0100

    Scanner Beispiel

---
+++ src/at/rangel/projects/MethodTester.java
@@ -0,0 +1,15 @@
+package at.rangel.projects;
+
+
+import java.util.Scanner;
+
+public class MethodTester {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+
+        int selection = scanner.nextInt();
+        System.out.println(selection);
+    }
+}
