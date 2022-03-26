0d25b351ffa3e7993973ae5922268d5dc9b99a7e
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-03-26 14:37:26 +0100

    Array #1

---
+++ src/at/rangel/projects/Array_1.java
@@ -0,0 +1,28 @@
+package at.rangel.projects;
+
+public class Array_1 {
+    public static void main(String[] args){
+        int[] sampleArray = new int[10];
+
+        sampleArray[0] = 10;
+
+        for (int i = 0; i < sampleArray.length; i++){
+            System.out.print(sampleArray[i] + " ");
+        }
+        System.out.println();
+        System.out.println("-------------------");
+
+        int[][] cellBoard = new int[3][3];
+
+        cellBoard[0][0] = 12;
+        cellBoard[2][1] = 34;
+
+        for (int row = 0; row < cellBoard.length; row++){
+            for (int col = 0; col < cellBoard.length; col++){
+                System.out.print(cellBoard[row][col] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
