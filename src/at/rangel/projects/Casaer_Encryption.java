335dc9b37064c0a15f8f16ec098b378ffcafdeae
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-03-31 16:39:46 +0200

    Casaer Encryption Part 1

--- src/at/rangel/projects/Casaer_Encryption.java
+++ src/at/rangel/projects/Casaer_Encryption.java
@@ -1,2 +1,19 @@
-package at.rangel.projects;public class Casaer_Encryption {
+package at.rangel.projects;
+
+import java.util.Scanner;
+
+public class Casaer_Encryption {
+
+    int selectionOfTranslationType = 0;
+    Scanner scanner = new Scanner(System.in);
+    String textToEncrypt = scanner.nextLine();
+    int shiftOfCharacters = 2;
+    char [] textArray = textToEncrypt.toCharArray();
+    char [] newTextArray = encrypt(shiftOfCharacters,textToEncrypt);
+
+    public static char[] encrypt(int offset, String charArray) {
+
+    }
 }
+
+
