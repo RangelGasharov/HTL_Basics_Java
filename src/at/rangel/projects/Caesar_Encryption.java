c58c9800c82515287354c07c3d81561a44c33f60
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-04-18 21:12:41 +0200

    Caesar_Encryption #1

--- src/at/rangel/projects/Caesar_Encryption.java
+++ src/at/rangel/projects/Caesar_Encryption.java
@@ -2,18 +2,50 @@ package at.rangel.projects;
 
 import java.util.Scanner;
 
-public class Caeser_Encryption {
+public class Caesar_Encryption {
+    public static void main(String[] args) {
+        System.out.println("Wählen Sie ihre gewünschte Funktion aus: ");
+        System.out.println("1 - Verschlüsseln");
+        System.out.println("2 - Entschlüsseln");
+        System.out.println("3 - Beenden");
+        System.out.println();
 
-    int selectionOfTranslationType = 0;
-    Scanner scanner = new Scanner(System.in);
-    String textToEncrypt = scanner.nextLine();
-    int shiftOfCharacters = 2;
-    char [] textArray = textToEncrypt.toCharArray();
-    char [] newTextArray = encrypt(shiftOfCharacters,textToEncrypt);
+        boolean isFinished = false;
+        Scanner scanner = new Scanner(System.in);
+        int selection = scanner.nextInt();
+        int key = 2;
 
-    public static char[] encrypt(int offset, String charArray) {
+        while (!isFinished) {
+            switch (selection) {
+                case 1:
+                    String text = scanner.nextLine();
+                    char[] chars = text.toCharArray();
 
+                    for (char c : chars) {
+                        c += key;
+                        System.out.print(c);
+                    }
+                    System.out.println();
+                    System.out.println("Geben Sie den Text an den Sie verschlüsseln wollen.");
+                    break;
+
+                case 2:
+                    text = scanner.nextLine();
+                    chars = text.toCharArray();
+
+                    for (char c : chars) {
+                        c -= key;
+                        System.out.print(c);
+                    }
+                    System.out.println();
+                    System.out.println("Geben Sie den Text an den Sie entschlüsseln wollen.");
+                    break;
+
+                case 3:
+                    System.out.println("Vorgang wird beendet...");
+                    isFinished = true;
+                    break;
+            }
+        }
     }
 }
-
-
