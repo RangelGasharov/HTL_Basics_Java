27387502ede4b4c899932fe603fce429507f1d54
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-05-05 15:52:25 +0200

    Cross_Sum_0 #1

---
+++ src/at/rangel/projects/Cross_sum_0.java
@@ -0,0 +1,16 @@
+package at.rangel.projects;
+
+public class Cross_sum_0 {
+    public static void main(String[] args) {
+        for (int cnt = 0; cnt < 1001; cnt++) {
+            String numberAsString = Integer.toString(cnt);
+            char[] numbers = numberAsString.toCharArray();
+
+            int sum = 0;
+            for (char number : numbers) {
+                sum = sum + Character.getNumericValue(number);
+            }
+            System.out.println(cnt + " - " + sum);
+        }
+    }
+}
