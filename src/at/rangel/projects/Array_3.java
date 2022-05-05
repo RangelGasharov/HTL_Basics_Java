6bfa4013da5d0aa47984e7d832176e9d63c0d1af
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-05-05 15:22:17 +0200

    Array_3 #1

--- src/at/rangel/projects/Array_3.java
+++ src/at/rangel/projects/Array_3.java
@@ -1,2 +1,18 @@
-package at.rangel.projects;public class Array_3 {
+package at.rangel.projects;
+
+import java.util.Random;
+
+public class Array_3 {
+    public static void main(String[] args) {
+        int[] array = new int[50];
+        int total = 0;
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(0, 101);
+            System.out.println("+ " + array[i]);
+            total += array[i];
+        }
+        System.out.println("= " + total);
+    }
 }
