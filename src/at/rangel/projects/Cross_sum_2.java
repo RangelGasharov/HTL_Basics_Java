b65b6075572a20854e8901e66faf4f9d22ee6940
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-05-05 16:38:39 +0200

    Cross_Sum_2 #1

--- src/at/rangel/projects/Cross_sum_2.java
+++ src/at/rangel/projects/Cross_sum_2.java
@@ -1,2 +1,20 @@
-package at.rangel.projects;public class Cross_sum_2 {
+package at.rangel.projects;
+
+public class Cross_sum_2 {
+    public static void main(String[] args) {
+        int number = 92414;
+
+        System.out.println(getCrossSum(number));
+    }
+
+    public static int getCrossSum(int number) {
+        int crossSum = 0;
+        while (number > 0) {
+            int last = number % 10;
+            crossSum += last;
+            number = number / 10;
+        }
+        return crossSum;
+    }
+
 }
