e58ce1700b7c735e4b9878b80c348460166bf79e
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-06-02 15:47:37 +0200

    Java_Test01 example 2 #1

--- src/tests/Test01_example2.java
+++ src/tests/Test01_example2.java
@@ -1,2 +1,24 @@
-package tests;public class Test01_example2 {
+package tests;
+
+import java.util.Random;
+
+public class Test01_example2 {
+    public static void main(String[] args){
+        Random random = new Random();
+        boolean isFinished = false;
+        int total = 0;
+
+        while(!isFinished){
+            int randomNumber = random.nextInt(0,11 );
+            if(randomNumber == 5){
+                System.out.println(randomNumber);
+                System.out.println("Summe: " + total);
+                isFinished = true;
+                break;
+            } else {
+                System.out.println("+ " + randomNumber);
+                total+= randomNumber;
+            }
+        }
+    }
 }
