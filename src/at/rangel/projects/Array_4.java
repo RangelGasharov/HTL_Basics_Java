cfed24fe8815023ddf4443f24ef6ead8d2b45410
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-06-09 16:27:28 +0200

    Array_4 #2

--- src/at/rangel/projects/Array_4.java
+++ src/at/rangel/projects/Array_4.java
@@ -1,7 +1,5 @@
 package at.rangel.projects;
 
-import groovy.json.JsonToken;
-
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -9,13 +7,13 @@ public class Array_4 {
 
     public static void main(String[] args) {
         int[] arrayToSort = {8, 3, 2, 22, 8, 1};
-        Integer[] arrayToSort2 = {9,212,29,43,12};
+        Integer[] arrayToSort2 = {9, 212, 29, 43, 12};
 
         Arrays.sort(arrayToSort);
         Arrays.sort(arrayToSort2, Collections.reverseOrder());
 
         System.out.println("Sorted Array #1: " + Arrays.toString(arrayToSort));
-        System.out.println("Reversed Array #2: "+  Arrays.toString(arrayToSort2));
+        System.out.println("Reversed Array #2: " + Arrays.toString(arrayToSort2));
     }
 
 }
