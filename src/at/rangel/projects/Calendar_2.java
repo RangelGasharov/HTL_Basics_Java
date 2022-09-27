fb9545a020ea6402ec3f32732e3939d02aec7e6a
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-09-27 17:49:12 +0200

    Calendar 2 #2

--- src/at/rangel/projects/Calendar_2.java
+++ src/at/rangel/projects/Calendar_2.java
@@ -1,24 +1,27 @@
 package at.rangel.projects;
 
 import java.time.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calendar_2 {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
 
     public static void main(String[] ars) {
 
         String[] nameOfTheDays = {"MON", "TUE", "WEN", "THU", "FRI", "SAT", "SUN"};
-        int[] calendar = new int[40];
+        int[] calendar = new int[42];
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please write down the number of the current month (1 = January, 2 = February, 3 = March, etc.");
-        int currentMonth = scanner.nextInt();
+        System.out.println("Please write down the " + ANSI_YELLOW + "number of the current month" + ANSI_RESET + "  (1 = January, 2 = February, 3 = March, etc.");
+        int currentMonth = input();
 
-        System.out.println("Please write down the first weekday of the current month (1 = Monday, 2 = Tuesday, 3 = Wednesday, etc. ");
-        int firstWeekDayOfMonth = scanner.nextInt() - 1;
+        System.out.println("Please write down the " + ANSI_YELLOW + "first weekday of the current month" + ANSI_RESET + " (1 = Monday, 2 = Tuesday, 3 = Wednesday, etc. ");
+        int firstWeekDayOfMonth = input();
 
-        System.out.println("Please write down the current year");
-        int currentYear = scanner.nextInt();
+        System.out.println("Please write down the " + ANSI_YELLOW + "current year" + ANSI_RESET);
+        int currentYear = input();
         boolean isLeapYear = currentYear % 4 == 0;
 
         Month month = Month.of(currentMonth);
@@ -58,17 +61,31 @@ public class Calendar_2 {
                 separator = "| ";
             }
 
-
             if (i == calendar.length - 1) {
                 endingSeparator = "  |\n";
             }
 
             if (j <= daysInMonth && j > 0) {
-                System.out.print(separator + j + endingSeparator);
+                System.out.print(separator + calendar[i] + endingSeparator);
             } else if (j > daysInMonth) {
                 System.out.print(separator + "  " + endingSeparator);
             } else {
                 System.out.print(separator + " " + endingSeparator);
+            }
+        }
+    }
+
+    public static int input() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                return (scanner.nextInt());
+            } catch (InputMismatchException e) {
+                scanner.next();
+                System.out.println("Please write down an integer and " + ANSI_RED + "NOT" + ANSI_RESET + " a letter , decimal number or others!");
+            } finally {
+                scanner.nextLine();
             }
         }
     }
