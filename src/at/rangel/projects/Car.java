d8aeef2ae947022fb2987df9428112d0f7188647
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-09-30 20:09:21 +0200

    Object Orientation #3

--- src/at/rangel/projects/Car.java
+++ src/at/rangel/projects/Car.java
@@ -1,17 +1,25 @@
 package at.rangel.projects;
 
 public class Car {
-    public int fuelConsumption;
-    public int fuelAmount;
+    public double fuelConsumption;
+    public double fuelAmount;
     public String brand;
     public String serialNumber;
+    public double reservoir;
     private String color;
+    private int horsePower;
 
-    public Car(int fuelConsumption, String brand, String serialNumber, int fuelAmount) {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+
+    public Car(int fuelConsumption, String brand, String serialNumber, int fuelAmount, int reservoir) {
         this.fuelConsumption = fuelConsumption;
         this.brand = brand;
         this.serialNumber = serialNumber;
         this.fuelAmount = fuelAmount;
+        this.reservoir = reservoir;
     }
 
     public void drive() {
@@ -22,25 +30,25 @@ public class Car {
     }
 
     public void toBreak() {
-        System.out.println("I brake!");
+        System.out.println(ANSI_RED + "I brake!" + ANSI_RESET);
     }
 
-    /* public void turboBoost() {
-        if (this.fuelAmount > fuelAmount / 10 && this.fuelAmount >= 0) {
-            System.out.println("SuperBoostMode");
+    public void turboBoost() {
+        if (this.fuelAmount > this.reservoir / 10) {
+            System.out.println(ANSI_BLUE + "SuperBoostMode" + ANSI_RESET);
         } else {
-            System.out.println("Not enough fuel to go to Super-boost");
+            System.out.println(ANSI_RED + "Not enough fuel to go to Super-boost" + ANSI_RESET);
         }
-    }*/
+    }
 
     public void honk(int amountOfRepetitions) {
         for (int i = 0; i < amountOfRepetitions; i++) {
-            System.out.println("Tutu");
+            System.out.println(ANSI_YELLOW + "Tutu!" + ANSI_RESET);
         }
     }
 
-    public int getRemainingRange() {
-        System.out.println("Remaining fuel: " + this.fuelAmount);
-        return 0;
+    public void getRemainingRange() {
+        double remainingRange = (this.fuelAmount / this.fuelConsumption) * 100;
+        System.out.println("Remaining distance in km: " + Math.round(remainingRange) + " km");
     }
 }
--- src/at/rangel/projects/ObjectOrientation_0.java
+++ src/at/rangel/projects/ObjectOrientation_0.java
@@ -3,20 +3,19 @@ package at.rangel.projects;
 public class ObjectOrientation_0 {
     public static void main(String[] args) {
 
-        Car c1 = new Car(7, "Audi", "A8237", 70);
+        Car c1 = new Car(7, "Audi", "A8237", 70, 270);
 
-        Car c2 = new Car(6, "Mercedes", "M2712", 20);
+        Car c2 = new Car(6, "Mercedes", "M2712", 80, 150);
 
-        Car c3 = new Car(5, "BMW", "B7310", 20);
+        Car c3 = new Car(5, "BMW", "B7310", 120, 120);
 
-        System.out.println("Fuel amount of car 2: " + c2.fuelAmount);
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 20; i++) {
             c2.drive();
             c2.getRemainingRange();
-            //c2.turboBoost();
+            c2.turboBoost();
+            System.out.println();
         }
 
-        System.out.println();
         c2.honk(3);
         c2.toBreak();
 
