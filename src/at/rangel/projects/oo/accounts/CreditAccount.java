b5b63528d8d381f6d9abdc5a5646e6f8812d5365
RangelGasharov <rangel.gasharov@student.htldornbirn.at>
2022-11-11 14:49:23 +0100

    Accounts #2

--- src/at/rangel/projects/oo/accounts/CreditAccount.java
+++ src/at/rangel/projects/oo/accounts/CreditAccount.java
@@ -1,4 +1,21 @@
 package at.rangel.projects.oo.accounts;
 
 public class CreditAccount {
+    private double balance;
+
+    public void takeCredit(double amount, double interestRate, double yearFactor) {
+        this.balance -= amount * Math.pow((1 + interestRate / 100), yearFactor);
+    }
+
+    public void payOffCredit(double amount) {
+        this.balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
--- src/at/rangel/projects/oo/accounts/Main.java
+++ src/at/rangel/projects/oo/accounts/Main.java
@@ -4,10 +4,16 @@ public class Main {
     public static void main(String[] args) {
         BaseAccount baseA1 = new BaseAccount();
         SavingsAccount savingA1 = new SavingsAccount();
+        CreditAccount creditA1 = new CreditAccount();
         System.out.println("Basic Account 1, balance: " + baseA1.getBalance());
         baseA1.withdraw(1000);
         System.out.println("Basic Account 1, balance: " + baseA1.getBalance());
 
+        savingA1.deposit(50);
+        System.out.println("Saving Account 1, balance: " + savingA1.getBalance());
         savingA1.withdraw(100);
+
+        creditA1.takeCredit(100000, 5, 10);
+        System.out.println("Credit Account 1, balance: " + creditA1.getBalance());
     }
 }
